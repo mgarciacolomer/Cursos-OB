26998b6711d401bfa8f47e82d246b131d3251afd
Marcos <mgarciacolomer@gmail.com>
2023-03-17 17:20:05 +0100

    Nyas

--- IntroProgramacion/EjerciciosTema9/src/Main.java
+++ IntroProgramacion/EjerciciosTema9/src/Main.java
@@ -6,16 +6,23 @@ public class Main {
         c.telefono = 654487911;
         c.credito = 465.11;
 
-        System.out.println("El cliente es: " + c.nombre + " de edad " + c.edad + ", telefono " + c.telefono
-                + " y dispone de un credito de " + c.credito);
+        System.out.println("DATOS DEL CLIENTE:");
+        System.out.println("Nombre: " + c.nombre);
+        System.out.println("Edad: " + c.edad);
+        System.out.println("Telefono: " + c.telefono);
+        System.out.println("Credito: " + c.credito);
+
         Trabajador t = new Trabajador();
         t.nombre = "Hermenegildo";
         t.edad = 33;
         t.telefono = 687444411;
         t.salario = 1825.01;
 
-        System.out.println("El trabajador es: " + t.nombre + " de edad " + t.edad + ", telefono " + t.telefono
-                + " y tiene un salario de " + t.salario);
+        System.out.println("DATOS DEL TRABAJADOR:");
+        System.out.println("Nombre: " + t.nombre);
+        System.out.println("Edad: " + t.edad);
+        System.out.println("Telefono: " + t.telefono);
+        System.out.println("Salario: " + t.salario);
     }
 }
 
